f6b6d52df7db281cddbda9eeed19ebb2e6f757ca
palewire <b@palewi.re>
2022-10-13 13:37:22 -0700

    Worked in @retry

--- tests/test_commands.py
+++ tests/test_commands.py
@@ -1,6 +1,9 @@
+from retry import retry
+
 from src import utils
 
 
+@retry()
 def test_download(tmpdir):
     """Test downloading."""
     url = "https://results.lavote.gov/electionresults/json?electionid=4269"
