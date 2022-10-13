bb46ffd6a1f2ef6c722c0a4972fa9eab2d75dc8b
palewire <b@palewi.re>
2022-10-13 14:06:39 -0700

    Write out raw files

--- src/download.py
+++ src/download.py
@@ -1,13 +1,22 @@
 import click
 
-from .utils import request_json
+import utils
 
 
 @click.command()
 def cli(electionid=4269):
     """Download JSON data posted by the L.A. County Registrar/Recorder."""
+    # Get the latest data
     url = f"https://results.lavote.gov/electionresults/json?electionid={electionid}"
-    click.echo(request_json(url))
+    data = utils.request_json(url)
+
+    # Write out a timestamped file
+    timestamp_path = utils.RAW_DATA_DIR / f"{utils.now().isoformat()}.json"
+    utils.write_json(data, timestamp_path)
+
+    # Overwrite the latest file
+    latest_path = utils.RAW_DATA_DIR / "latest.json"
+    utils.write_json(data, latest_path)
 
 
 if __name__ == "__main__":
--- src/utils.py
+++ src/utils.py
@@ -1,10 +1,33 @@
+import json
+import pathlib
 import typing
+from datetime import datetime
 
+import pytz
 import requests
+from rich import print
+
+THIS_DIR = pathlib.Path(__file__).parent.absolute()
+ROOT_DIR = THIS_DIR.parent
+DATA_DIR = ROOT_DIR / "data"
+RAW_DATA_DIR = DATA_DIR / "raw"
+
+
+def now() -> datetime:
+    """Return the current time in our local timezone."""
+    now = datetime.now()
+    tz = pytz.timezone("America/Los_Angeles")
+    return now.astimezone(tz)
 
 
 def request_json(url: str) -> typing.Dict:
     """Request the provided URL and return the JSON response as a Python dictionary."""
+    print(f"🌐 Requesting JSON from {url}")
     r = requests.get(url)
     assert r.ok
     return r.json()
+
+
+def write_json(data: typing.Dict, path: pathlib.Path, indent: int = 2):
+    print(f"✏️ Writing JSON to {path}")
+    json.dump(data, open(path, "w"), indent=indent)
